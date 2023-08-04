6bf1fe9381d2ec04aaeb3f474c9e8d3349869826
githubscientist <sathish.datascientist@gmail.com>
2023-08-04 21:35:48 +0530

    codekata day 1

---
+++ codekata/day1/operators.js
@@ -0,0 +1,63 @@
+/*
+    = assignment operator
+
+    arithmetic operators
+        + addition
+        - subtraction
+        * multiplication
+        / division
+        ** exponent
+*/
+
+// console.log(100 ** 0.5);
+
+/*
+    relational operators
+        < less than
+        > greater than
+        <= less than or equal to
+        >= greater than or equal to
+        == double equal to (comparison operator)
+        != not equal to
+        === triple equal to (comparison with type checking)
+        !== not double equal to (type checking)
+*/
+
+// console.log(6 === '6');
+
+/*
+    logical operators
+        && logical and
+        || logical or
+        ! logical not
+*/
+
+// let age = 14;
+
+// /*
+//     Eligibility: age should be greater than or equal to 18
+// */
+
+// console.log(age >= 18);
+
+// let age = 17;
+// let hasAadhar = true;
+// let hasVoterID = false;
+
+/*
+    Eligibility: 
+        1. age should be greater than or equal to 18
+        2. should have either aadhar card or voter id
+*/
+
+// console.log(age >= 18 && (hasAadhar || hasVoterID));
+
+// console.log(!(5 > 6));
+
+// console.log(5 + 3 * 4 - 2);
+
+// console.log(5 == '5');
+
+// let hasAadhar = true;
+
+// console.log(!hasAadhar);
---
+++ codekata/day1/variables.js
@@ -0,0 +1,82 @@
+// console.log('welcome students');
+
+// variables
+// a label
+// every byte has a memory address (hexadecimal format 0x543efa)
+
+// let age = 25;
+
+// age = 36;
+
+// age = 40;
+
+// console.log(age);
+
+// comments: to describe a particular code -> single line comment
+/*
+    multi-line comment
+
+*/
+
+let age = 25;
+
+// datatype: defines the type of data we store in a variable
+/*
+    1. Number
+        - Integers: 0, 34, 125, 7321, 12, 1, ....
+        - Floting point values: 23.67, 12.12, 19.31, 4.78, ....
+    2. Boolean
+        - true
+        - false
+    3. String
+        - sequence of characters
+        - alphabets [ lowercase, uppercase ], numbers, special characters
+        - character or word or sentence or multiple sentences
+    4. Object
+        - Arrays: List of values comma separated and wrapped inside square brackets
+        - Javascript Object: Key value pairs comma separated
+*/
+
+// console.log(typeof (age));
+
+let percentage = 23.67;
+
+// console.log(typeof (percentage));
+
+let isEligible = false;
+
+// console.log(typeof (isEligible));
+
+let fruitName = 'apple is a fruit';
+let fruitName1 = "apple is a fruit and it costs 180.34 rupees";
+let fruitName2 = `apple is a fruit and the password is apple@123`;
+
+// console.log(typeof (fruitName));
+
+let scores = [100, 93, 85, 99, 56, 78];
+
+// console.log(typeof (scores));
+
+let student101 = ['sathish', 30, 'weekday', 'tamil', '500', '255', 63.12, false, ['st1', 'ar3', 'ar5']];
+
+// console.log(typeof (student101));
+
+// the following defines a object value of a student
+let student102 = {
+    name: 'sathish', 
+    age: 30,
+    classType: 'weekday',
+    language: 'tamil',
+    geekCoins: 500,
+    runCode: 255,
+    percentage: 63.12,
+    isEligible: false,
+    codekata: {
+        string: [1, 2],
+        array: [3, 5],
+    }
+}
+
+// console.log(typeof (student101));
+
+let firstName = 'krish';
