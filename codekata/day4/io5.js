e510594150c2f129b4c2f5e3e51a05b5d8d6aaa8
githubscientist <sathish.datascientist@gmail.com>
2023-08-09 21:25:32 +0530

    codekata day 4

---
+++ codekata/day4/io5.js
@@ -0,0 +1,24 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic
+
+//   console.log(userInput[0], userInput[1], userInput[2]);
+    console.log(userInput[0]);
+    console.log(userInput[1]);
+    console.log(userInput[2]);
+
+  //end-here
+});
