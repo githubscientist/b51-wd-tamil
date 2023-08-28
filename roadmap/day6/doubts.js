f45d6b54b0d78704458b57955c67111bf4c92c1f
githubscientist <sathish.datascientist@gmail.com>
2023-08-28 21:35:10 +0530

    day 6

---
+++ roadmap/day6/doubts.js
@@ -0,0 +1,40 @@
+// For the given JSON, iterate over all for loops (for, for in, for of, forEach)
+
+// json string
+// let jsonString = '{"title":"dsa","author":"sathish","price":2000,"availability":true,"isbn":"ISB123"}';
+
+// let jsonObject = JSON.parse(jsonString);
+
+// for (let index = 0; index < (Object.keys(jsonObject)).length; index++){
+//     console.log(Object.keys(jsonObject)[index], ':', jsonObject[Object.keys(jsonObject)[index]]);
+// }
+
+// for (let index in Object.keys(jsonObject)){
+//     console.log(Object.keys(jsonObject)[index], ':', jsonObject[Object.keys(jsonObject)[index]]);
+// }
+
+// console.log(Object.entries(jsonObject));
+
+// for (let entry of Object.entries(jsonObject)){
+//     console.log(entry[0], ':', entry[1]);
+// }
+
+// Object.entries(jsonObject).forEach((entry) => {
+//     console.log(entry[0], ':', entry[1]);
+// });
+
+// Object.entries(jsonObject).forEach((entry) => {
+//     console.log(entry[0], ':', entry[1]);
+// });
+
+// function print() {
+//     return 'hello';
+// }
+
+// console.log(print());
+
+// function print() {
+//     return 'hello';
+// }
+
+// console.log(print());
