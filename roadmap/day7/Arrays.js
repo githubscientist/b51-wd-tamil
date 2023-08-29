e6c6d6627e4d87f4ee5900ee495bd8b43c17efd0
githubscientist <sathish.datascientist@gmail.com>
2023-08-29 11:08:17 +0530

    day 7

--- roadmap/day7/Arrays.js
+++ roadmap/day7/Arrays.js
@@ -1,26 +1,28 @@
-let numbers = [1, 2, 4];
+// let numbers = [1, 2, 4];
 
-numbers.push(6);
+// numbers.push(6);
 
-numbers.pop();
+// numbers.pop();
 
-numbers.unshift(10);
+// numbers.unshift(10);
 
-numbers.shift();
+// numbers.shift();
 
-// console.log(numbers.slice(3, ));
+// // console.log(numbers.slice(3, ));
 
-numbers.splice(2, 0, 3);
-numbers.splice(2, 0, 23);
-numbers.splice(4, 0, 34);
+// numbers.splice(2, 0, 3);
+// numbers.splice(2, 0, 23);
+// numbers.splice(4, 0, 34);
 
-numbers.splice(4, 1);
-numbers.splice(2, 1);
+// numbers.splice(4, 1);
+// numbers.splice(2, 1);
 
-// console.log(numbers);
+// // console.log(numbers);
 
-// let copiedNumbers = Array.from(numbers);
+// // let copiedNumbers = Array.from(numbers);
 
-// console.log(copiedNumbers);
+// // console.log(copiedNumbers);
 
-console.log(numbers.indexOf(30));
+// console.log(numbers.indexOf(30));
+
+console.log([1, 2, 4].join(','));
--- roadmap/day7/Book.js
+++ roadmap/day7/Book.js
@@ -16,7 +16,7 @@ class Book {
 
     // setTitle(title) {
     //     this.title = title;
-    // }bot
+    // }
 
     get title() {
         return `title: ${this._title}`;
