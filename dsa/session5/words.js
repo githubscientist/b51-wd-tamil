35a84899dd80571e6e160e2f5288ca36d0ba71fb
githubscientist <sathish.datascientist@gmail.com>
2023-09-01 21:40:40 +0530

    session 5

--- dsa/session5/words.js
+++ dsa/session5/words.js
@@ -10,25 +10,25 @@
 
 // ------------------------------------------------
 
-let words = ['apple', 'banana', 'orange', 'apple', 'apple', 'gouva', 'pineapple', 'orange'];
+// let words = ['apple', 'banana', 'orange', 'apple', 'apple', 'gouva', 'pineapple', 'orange'];
 
-let frequencies = {};
+// let frequencies = {};
 
-for (let index = 0; index < words.length; index++){
-    frequencies[words[index]] = 0;
-}
-
-for (let index = 0; index < words.length; index++){
-    frequencies[words[index]]++;
-}
-
-console.log(Object.entries(frequencies).filter(value => value[1]==1).map(value => value[0]));
-
-// let person = {
-//     name: 'sathish',
-//     age: 30
+// for (let index = 0; index < words.length; index++){
+//     frequencies[words[index]] = 0;
 // }
 
-// person['phone'] = '9080';
+// for (let index = 0; index < words.length; index++){
+//     frequencies[words[index]]++;
+// }
 
-// console.log(person);
+// console.log(Object.entries(frequencies).filter(value => value[1]==1).map(value => value[0]));
+
+let person = {
+    name: 'sathish',
+    age: 30
+}
+
+person['name'] = 'sathish';
+
+console.log(person);
