0b53d6f599cf095c8ad6634d1985e14cdaf4e903
githubscientist <sathish.datascientist@gmail.com>
2023-09-07 21:35:08 +0530

    day 9 css

---
+++ roadmap/day9-css/tasks.js
@@ -0,0 +1,24 @@
+/*
+    Exercise 1:
+
+    - Create an HTML file with the filename 'index.html'
+    - set the document title to 'My First Web Page'
+    - Add an h1 element with the text "Welcome to My Web Page"
+    - Add a paragraph with the text "This is a basic HTML Page" below the header.
+    - change the header element's color to "blue" and its font to "Arial, Sans-Serif"
+    - change the paragraph element color to "green" and its font to "Times New Roman, Serif"
+    - Create a unordered list with three list elements "Item 1", "Item 2", "Item 3"
+    - Set the unordered list items type to "circle"
+    - Create an anchor element with the link "https://www.google.com" with the text "Visit Google"
+    - change the color of the link to red
+    - remove the underline from the link text
+    - Create a div element with the class "my-div"
+    - Add two paragraphs inside the div with the class "my-div"
+    - add a 2px solid type border to the div element
+    - set the background of the div to #f0f0f0
+    - create a form with an input text element and set the placeholder to "enter your name"
+    - add a button with the text "Submit"
+    - center alig the content inside the form
+    - change the width of the text input box to 200px
+    - set the background of the submit button to #007bff and text color to #fff
+*/
