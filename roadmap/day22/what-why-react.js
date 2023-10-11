daf21cbf2907e916b441b96e50f5c8dba2c6351b
githubscientist <sathish.datascientist@gmail.com>
2023-10-11 21:26:07 +0530

    day 22

---
+++ roadmap/day22/what-why-react.js
@@ -0,0 +1,20 @@
+/*
+    React 
+
+    - Its a popular JavaScript library
+    - It is used to build user interfaces & to create dynamic web applications
+    - It is developed and maintained by Facebook
+    
+    Features:
+    
+    - Component Based Architecture
+        Component is a piece of UI or a group of UI elements
+        Reusability - easy to maintain and manage
+    
+    - Virtual DOM
+        to optimize the updates and rendering
+        it will calculate the difference between the previous DOM and the virtual DOM to update the actual DOM only where necessary.
+    
+    - JSX (JavaScript XML)
+        It allows us to write HTML code within the javascript files.
+*/
